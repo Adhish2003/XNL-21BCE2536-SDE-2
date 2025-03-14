02ca2b02180f64a847f12a50e6d11de4feb821c9
Adhish2003 <adhishadhish001@gmail.com>
2025-03-14 20:11:23 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+// SETUP
+dotenv.config({ path: "./server/.env" });
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser("secretcode"));
+
+// DATABASE
+const URI = "mongodb://127.0.0.1:27017/sde"
+const connectDB = async () => {
+  await mongoose.connect(URI)
+}
+
+connectDB()
+  .then(() => {
+    console.log("MongoDB connected")
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch(() => {
+    console.log("Cannot connect to DB")
+  })
+
+// ROUTES
+const authRouter = require("./routes/authRoutes");
+const dataRouter = require("./routes/dataRoutes");
+const stockRouter = require("./routes/stockRoutes");
+
+app.use("/api/auth", authRouter);
+app.use("/api/data", dataRouter);
+app.use("/api/stock", stockRouter);
+
+
+// APP
+
